9c63cc32ac1ed9dc8a1673e3e32d57396d8cb855
Asif032 <asif01981872223@gmail.com>
2023-03-10 15:40:02 +0600

    linked register and login acitvity with main activity

--- app/src/main/java/com/example/food_engine/RegistrationActivity.java
+++ app/src/main/java/com/example/food_engine/RegistrationActivity.java
@@ -5,16 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.food_engine.databinding.ActivityRegistrationBinding;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    @Override
+    ActivityRegistrationBinding binding;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     public void signin(View view) {
         startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+    }
+
+    public void regComplete(View view) {
+        String userName = binding.username.getText().toString();
+        String phoneNumber = binding.phoneNumber.getText().toString();
+        String password = binding.password.getText().toString();
+
+        if (userName.isEmpty() || phoneNumber.isEmpty()) {
+            Toast.makeText(RegistrationActivity.this, "Name or phone number can't be empty", Toast.LENGTH_SHORT).show();
+        } else if (password.length() < 6) {
+            Toast.makeText(RegistrationActivity.this, "Password must be atleast 6 characters", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+            intent.putExtra("type", 1);
+            intent.putExtra("userName", userName);
+            intent.putExtra("phoneNumber", phoneNumber);
+            intent.putExtra("password", password);
+
+            startActivity(intent);
+        }
     }
 }
